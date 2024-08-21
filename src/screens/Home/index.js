238030eef3dc6785123575a6094e725a47d5900c
unknown <you@example.com>
2024-08-21 12:09:35 -0300

    requisição

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,8 +5,30 @@ import CardMovies from '../../components/cardMovies';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchbar';
 import Filmes from '../../data/movies'
+import { useEffect,useState } from 'react';
 
 export default function App() {
+
+  const [movies,setMovies] = useState();
+
+  
+useEffect(()=>{
+
+  async function buscarFilmes(){
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=5d7a174ab32583655f441728ae72d5e4&language=pt-BR&page=1')
+    const data = await response.json()
+    console.log(data.results)
+  }
+
+  buscarFilmes();
+
+
+
+  
+},[]  )
+
+
+
   return (
     <View style={styles.container}>
      <Header></Header>
